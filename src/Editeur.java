098ae02427eeb94df977a827bc5c0b6cfdee0d44
loris <loris_labarre@outlook.fr>
2023-03-30 16:04:37 +0200

    Mise en place du bonus Scanner

--- src/Editeur.java
+++ src/Editeur.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Editeur {
     public static void main(String[] args) {
-        JavaClasse calculatrice = new JavaClasse("calculatrice");
-        calculatrice.setAttributProtege(new JavaMethode("calculer"));
-        JavaAttribut typeOperation = new JavaAttribut("type opération");
+
+        Scanner lectureClavier = new Scanner(System.in);
+        System.out.println("Saisissez le nom de la Classe");
+        JavaClasse calculatrice = new JavaClasse(lectureClavier.nextLine());
+        System.out.println("Saisissez la méthode");
+        calculatrice.setAttributProtege(new JavaMethode(lectureClavier.nextLine()));
+        System.out.println("Saisissez le type d'opération");
+        JavaAttribut typeOperation = new JavaAttribut(lectureClavier.nextLine());
         calculatrice.afficherNom();
         calculatrice.getAttributProtege().afficherNom();
         typeOperation.afficherNom();
